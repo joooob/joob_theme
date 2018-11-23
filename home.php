6ff6543770ec0dfd55130d0c83b1bbcacb33bf48
Sébastien <sebastien.daniel@hotmail.com>
2018-11-23 12:11:38 +0100

    develop

---
+++ home.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Accueil</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+	<h1>Recherchez</h1>
+        <div>
+          <input type="text" class="searchTerm" placeholder="type d'emplois">
+        </div>
+        <br/>
+        <div>
+          <input type="text" class="searchTerm" placeholder="Ville/Département/Région">
+        </div>
+        <br/>
+      <div id="radio_buttons">
+        <div>
+          <input type="radio" id="huey" name="drone" value="huey"
+                 checked>
+          <label for="huey">CDD</label>
+        </div>
+        <div>
+          <input type="radio" id="huey" name="drone" value="huey"
+                 checked>
+          <label for="huey">CDI</label>
+        </div>
+        <div>
+          <input type="radio" id="huey" name="drone" value="huey"
+                 checked>
+          <label for="huey">Intérim</label>
+        </div>
+        <div>
+          <input type="radio" id="huey" name="drone" value="huey"
+                 checked>
+          <label for="huey">Freelance</label>
+        </div>
+      </div>
+      <br/>
+      <input type="submit" value="Valider">
+</body>
+</html>
---
+++ inscription.html
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Inscription</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+	<section data-section="section1">
+		<h1>Inscrivez vous pour trouver un emploie ou un employeur qui vous correspond ! </h1>
+		<p>Déjà un compte? <span>Connectez-vous</span></p>
+	</section>
+	<section data-section="section2">
+		 <form id="FormulaireInscription">
+        <fieldset>
+            <legend>Se connecter avec</legend>
+            <p>
+                <label for="email">Votre email</label>
+                <input type="email" />
+                <br/>
+                <label for="pass">Votre mot de passe </label>
+                <input type="password" name="pass" id="pass" minlength="6"/>   
+                <br/>
+                <label for="pass">Confirmation du mot de passe</label>
+                <input type="password" name="pass2" id="pass2" minlength="6"/>              
+
+
+            </p>
+            <p>
+                <input class="submit" type="submit" value="S'inscrire">
+            </p>
+        </fieldset>
+    </form>
+
+</body>
+</html>
