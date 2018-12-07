a187f9fe4b953c837d17b44c12fd36f5cfccde89
Sébastien <sebastien.daniel@hotmail.com>
2018-12-07 16:07:40 +0100

    update

--- home.php
+++ home.php
@@ -1,8 +1,49 @@
-<!DOCTYPE html>
-<html>
-  <?php include("header.php"); ?>
-  <body>
-    <h1>Recherchez</h1>
-
-  </body>
+<?php
+get_header();
+?>
+<head>
+  <meta charset="utf-8">
+  <title>Accueil</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <div class="accueil">
+	<h1>Recherchez</h1>
+        <div>
+          <input type="text" class="searchTerm" placeholder="type d'emplois">
+        </div>
+        <br/>
+        <div>
+          <input type="text" class="searchTerm" placeholder="Ville/Département/Région">
+        </div>
+        <br/>
+      <div id="radio_buttons">
+        <div>
+          <input type="radio" id="huey" name="drone" value="huey"
+                 checked>
+          <label for="huey">CDD</label>
+        </div>
+        <div>
+          <input type="radio" id="huey" name="drone" value="huey"
+                 checked>
+          <label for="huey">CDI</label>
+        </div>
+        <div>
+          <input type="radio" id="huey" name="drone" value="huey"
+                 checked>
+          <label for="huey">Intérim</label>
+        </div>
+        <div>
+          <input type="radio" id="huey" name="drone" value="huey"
+                 checked>
+          <label for="huey">Freelance</label>
+        </div>
+      </div>
+      <br/>
+      <input type="submit" value="Valider">
+    </div>
+</body>
 </html>
+<?php
+get_footer();
